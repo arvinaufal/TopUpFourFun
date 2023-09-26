b025d4163a5ca0a5f3a4a98f0875457163feaa28
dewabrata15 <muhammaddewabrata@gmail.com>
2023-09-26 21:03:07 +0700

    dewa

---
+++ assets/index.js
@@ -0,0 +1,72 @@
+let games = [
+    {
+        nama : 'Clash of Clans' ,
+        logo : './Logo/downloaded.jpeg' ,
+        harga: 111
+    },
+    {
+        nama : 'Free Fire' ,
+        logo : './Logo/free fire.png' ,
+        harga: 111
+    },
+    {
+        nama : 'Growtopia' ,
+        logo : './Logo/growtopia.png' ,
+        harga: 111
+    },
+    {
+        nama : 'Call of Duty' ,
+        logo : './Logo/images.jpeg' ,
+        harga: 111
+    },
+    {
+        nama : 'Mobile Legends' ,
+        logo : './Logo/ML.png' ,
+        harga: 111
+    },
+    {
+        nama : 'State of Survival' ,
+        logo : './Logo/state of survival.png' ,
+        harga: 111
+    },
+    {
+        nama : 'Valorant' ,
+        logo : './Logo/valorant.jpeg' ,
+        harga: 111
+    },
+    {
+        nama : 'Clash Royale' ,
+        logo : './Logo/clashroyale.jpeg' ,
+        harga: 111
+    },
+    {
+        nama : 'Hogwarts Legacy' ,
+        logo : './Logo/hogwarts.webp' ,
+        harga: 111
+    },
+    {
+        nama : 'Stumble Guys' ,
+        logo : './Logo/stumble.png' ,
+        harga: 111
+    },
+    {
+        nama : 'PUBG Mobile' ,
+        logo : './Logo/pubg.png' ,
+        harga: 111
+    },
+    {
+        nama : 'Hay Day' ,
+        logo : './Logo/hay day.jpeg' ,
+        harga: 111
+    },
+    {
+        nama : 'Speed Drifters' ,
+        logo : './Logo/speed drifters.jpeg' ,
+        harga: 111
+    },
+    {
+        nama : 'Brawl Stars' ,
+        logo : './Logo/brawl.jpeg' ,
+        harga: 111
+    },
+]
