1effbebfa224c0f46876d9095e99f9e24e7dd2f2
dewabrata15 <muhammaddewabrata@gmail.com>
2023-09-27 20:40:30 +0700

    dewa

--- assets/index.js
+++ assets/index.js
@@ -1,98 +1,111 @@
 let games = [
     {
+        id : 1, 
         nama : 'Clash of Clans' ,
-        logo : './Logo/downloaded.jpeg' ,
+        logo : './games/downloaded.jpeg' ,
         genre : 'Online multiplayer strategy game',
         item : 'Diamonds',
         harga: 111
     },
     {
+        id : 2,
         nama : 'Free Fire' ,
-        logo : './Logo/free fire.png' ,
+        logo : './games/free fire.png' ,
         genre : 'Battle Royale',
         item : 'Cash',
         harga: 111
     },
     {
+        id : 3,
         nama : 'Growtopia' ,
-        logo : './Logo/growtopia.png' ,
+        logo : './games/growtopia.png' ,
         genre : 'Multiplayer online sandbox',
         item : 'Gems',
         harga: 111
     },
     {
+        id : 4,
         nama : 'Call of Duty Mobile' ,
-        logo : './Logo/images.jpeg' ,
+        logo : './games/images.jpeg' ,
         genre : 'Battle Royale',
         item : 'Credits',
         harga: 111
     },
     {
         nama : 'Mobile Legends' ,
-        logo : './Logo/ML.png' ,
+        logo : './games/ML.png' ,
         genre : 'Multiplayer online battle arena ',
         item : 'Diamonds',
         harga: 111
     },
     {
+        id : 4,
         nama : 'State of Survival' ,
-        logo : './Logo/state of survival.png' ,
+        logo : './games/state of survival.png' ,
         genre : 'Online mobile strategy game',
         item : 'Golds',
         harga: 111
     },
     {
+        id : 5,
         nama : 'Valorant' ,
-        logo : './Logo/valorant.jpeg' ,
+        logo : './games/valorant.jpeg' ,
         genre : 'Battle Royale',
         item : 'Credits',
         harga: 111
     },
     {
+        id : 6,
         nama : 'Clash Royale' ,
-        logo : './Logo/clashroyale.jpeg' ,
+        logo : './games/clashroyale.jpeg' ,
         genre : 'Online multiplayer strategy game',
         item : 'Diamonds',
         harga: 111
     },
     {
+        id : 7,
         nama : 'Hogwarts Legacy' ,
-        logo : './Logo/hogwarts.webp' ,
+        logo : './games/hogwarts.webp' ,
         genre : 'Open world game',
         item : 'Coins',
         harga: 111
     },
     {
+        id : 8,
         nama : 'Stumble Guys' ,
-        logo : './Logo/stumble.png' ,
+        logo : './games/stumble.png' ,
         genre : 'Multiplayer party knockout game ',
         item : 'Gems',
         harga: 111
     },
     {
+        id : 9,
         nama : 'PUBG Mobile' ,
-        logo : './Logo/pubg.png' ,
+        logo : './games/pubg.png' ,
         genre : 'Battle Royale',
         item : 'Credits',
         harga: 111
     },
     {
+        id : 10,
         nama : 'Hay Day' ,
-        logo : './Logo/hay day.jpeg' ,
+        logo : './games/hay day.jpeg' ,
         genre : 'Online multiplayer strategy game',
         item : 'Diamonds',
         harga: 111
     },
     {
+        id : 11,
         nama : 'Speed Drifters' ,
-        logo : './Logo/speed drifters.jpeg' ,
+        logo : './games/speed drifters.jpeg' ,
         genre : 'Online racing game',
         item : 'Diamonds',
         harga: 111
     },
     {
+        id : 12,
         nama : 'Brawl Stars' ,
-        logo : './Logo/brawl.jpeg' ,
+        logo : './games/brawl.jpeg' ,
         genre : 'Multiplayer online battle arena',
         item : 'Gems',
         harga: 111
