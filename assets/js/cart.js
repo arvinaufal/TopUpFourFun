85c4659781c4758f8a488f69b5678f1de04fe844
arvinaufal <arvinaufalagustian@gmail.com>
2023-09-28 21:25:22 +0700

    CRUD User ID Done

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -31,12 +31,40 @@ function getToken(){
 
 
 function crudPlayerId(playerId) {
+    let cart = JSON.parse(localStorage.getItem('cart'));
+    let gameId = cart.gameId;
+    let games = JSON.parse(localStorage.getItem('games'));
+    let item = null;
+    for (const i of games) {
+     
+        if (i.id == gameId) {
+            item = i.item;
+            break;
+        }
+    }
+
+    let jmlDiamond = 0;
+    for (let i = 0; i < cart.playerId.length; i++) {
+        if (cart.playerId[i].userId === playerId) {
+            jmlDiamond = cart.playerId[i].jmlDm
+            break;
+        }
+    }
     Swal.fire({
         title: 'Form Detail User',
         html: `
-            <input type="text" id="usernameSA" class="swal2-input" placeholder="UserId" value="${playerId}"> 
-            <input type="number" id="jmlDiamondSA" class="swal2-input" placeholder="Jumlah Diamond">
+            <input type="text" id="usernameOld" class="swal2-input" placeholder="UserId" value="${playerId}" hidden>
+            <div>
+                <label for="usernameSA">User ID</label>
+                <input type="text" id="usernameSA" class="swal2-input" placeholder="UserId" value="${playerId}">
+            </div>
+            <div>
+                <label for="jmlDiamondSA">Jml ${item}</label>
+                <input type="number" id="jmlDiamondSA" class="swal2-input" placeholder="Jumlah Diamond" value="${jmlDiamond}" min="0">
+            </div>
         `,
+
+    
 
         showCancelButton: true,
         showConfirmButton: true,
@@ -47,28 +75,153 @@ function crudPlayerId(playerId) {
         focusConfirm: false,
     }).then((result) => {
         if (result.isConfirmed) {
+            const usernameOld = Swal.getPopup().querySelector('#usernameOld').value;
             const username = Swal.getPopup().querySelector('#usernameSA').value;
             const jmlDiamond = Swal.getPopup().querySelector('#jmlDiamondSA').value;
 
             // Panggil fungsi untuk meng-handle update data
-            handleUpdate(username, jmlDiamond);
+            handleUpdate(username, usernameOld, jmlDiamond, item);
         } else if (result.dismiss === Swal.DismissReason.cancel) {
             // Panggil fungsi untuk meng-handle delete data
-            handleDelete();
+            handleDelete(playerId);
         }
     });
 }
 
-function handleUpdate(username, jmlDiamond) {
-    // Lakukan apa yang perlu dilakukan untuk memproses update
-    console.log('Update data:');
-    console.log(`Username: ${username}, Jumlah Diamond: ${jmlDiamond}`);
+function handleUpdate(username, usernameOld, jmlDiamond, item) {
+    let countUsername = (username+"").length;
+
+    if(jmlDiamond*1 < 0){
+        Swal.fire({
+            title: 'Gagal!',
+            text: "Jumlah "+ item +" tidak boleh negatif!",
+            icon: 'error',
+            showCancelButton: false,
+            confirmButtonColor: '#3085d6',
+            confirmButtonText: 'Ok',
+            allowOutsideClick: false,
+            allowEscapeKey: false,
+            allowEnterKey: false,
+        });
+
+        return;
+    }
+
+    if(countUsername < 9){
+        Swal.fire({
+            title: 'Gagal!',
+            text: "User ID minimal 9 digit!",
+            icon: 'error',
+            showCancelButton: false,
+            confirmButtonColor: '#3085d6',
+            confirmButtonText: 'Ok',
+            allowOutsideClick: false,
+            allowEscapeKey: false,
+            allowEnterKey: false,
+        });
+
+    }else if(countUsername > 10){
+
+        Swal.fire({
+            title: 'Gagal!',
+            text: "User ID maksimal 10 digit!",
+            icon: 'error',
+            showCancelButton: false,
+            confirmButtonColor: '#3085d6',
+            confirmButtonText: 'Ok',
+            allowOutsideClick: false,
+            allowEscapeKey: false,
+            allowEnterKey: false,
+        });
+
+    }else{
+
+        let cart = JSON.parse(localStorage.getItem('cart'));
+        for (let i = 0; i < cart.playerId.length; i++) {
+            if (cart.playerId[i].userId === usernameOld) {
+                cart.playerId[i].userId = username;
+    
+                if (jmlDiamond) {
+                    cart.playerId[i].jmlDm = jmlDiamond*1;
+                }
+                break;
+            }
+        }
+    
+        localStorage.setItem('cart', JSON.stringify(cart));
+        showPlayerId();
+
+
+        Swal.fire({
+            title: 'Berhasil!',
+            text: "Berhasil mengupdate data User!",
+            icon: 'success',
+            showCancelButton: false,
+            confirmButtonColor: '#3085d6',
+            confirmButtonText: 'Ok',
+            allowOutsideClick: false,
+            allowEscapeKey: false,
+            allowEnterKey: false,
+        });
+
+    }
+
+
+
 }
 
-function handleDelete() {
-    // Lakukan apa yang perlu dilakukan untuk memproses delete
-    console.log('Delete data');
+
+
+function handleDelete(playerId) {
+    let cart = JSON.parse(localStorage.getItem('cart'));
+
+    // Temukan indeks dari playerId yang ingin dihapus
+    let indexToRemove = -1;
+    for (let i = 0; i < cart.playerId.length; i++) {
+        if (cart.playerId[i].userId === playerId) {
+            indexToRemove = i;
+            break;
+        }
+    }
+
+    if (indexToRemove !== -1) {
+        // Hapus elemen dari array
+        cart.playerId.splice(indexToRemove, 1);
+
+        // Perbarui localStorage
+        localStorage.setItem('cart', JSON.stringify(cart));
+
+        // Tampilkan ulang daftar player ID
+        showPlayerId();
+
+        // Tampilkan pesan sukses
+        Swal.fire({
+            title: 'Berhasil!',
+            text: "Berhasil menghapus data User!",
+            icon: 'success',
+            showCancelButton: false,
+            confirmButtonColor: '#3085d6',
+            confirmButtonText: 'Ok',
+            allowOutsideClick: false,
+            allowEscapeKey: false,
+            allowEnterKey: false,
+        });
+    } else {
+        // Jika playerId tidak ditemukan
+        Swal.fire({
+            title: 'Gagal!',
+            text: "User ID tidak ditemukan!",
+            icon: 'error',
+            showCancelButton: false,
+            confirmButtonColor: '#3085d6',
+            confirmButtonText: 'Ok',
+            allowOutsideClick: false,
+            allowEscapeKey: false,
+            allowEnterKey: false,
+        });
+    }
 }
+
 
 
 function showPlayerId() {
@@ -80,8 +233,8 @@ function showPlayerId() {
     for (const pI of playerIds) {
         temp += 
         `
-            <div class="btn btn-success" onclick="crudPlayerId('${pI}')">
-                ${pI}
+            <div class="btn btn-success" onclick="crudPlayerId('${pI.userId}')">
+                ${pI.userId}
             </div> 
         `;
     }
@@ -141,7 +294,7 @@ function showUserLoginStatus(){
                 allowEscapeKey: false,
                 allowEnterKey: false,
             });
-            
+
         }else if(countUserId > 10){
 
             Swal.fire({
@@ -160,7 +313,7 @@ function showUserLoginStatus(){
 
             Swal.fire({
                 title: 'Yakin?',
-                text: "Apakah Anda yakin ingin menambahkan user id " + userId + "?",
+                text: "Apakah Anda yakin ingin menambahkan User ID " + userId + "?",
                 icon: 'question',
                 showCancelButton: true,
                 confirmButtonColor: '#3085d6',
@@ -189,7 +342,7 @@ function showUserLoginStatus(){
                         
                         Swal.fire({
                             title: 'Gagal!',
-                            text: "Player ID sudah ditambahkan sebelumnya!",
+                            text: "User ID sudah ditambahkan sebelumnya!",
                             icon: 'error',
                             showCancelButton: false,
                             confirmButtonColor: '#3085d6',
@@ -204,14 +357,12 @@ function showUserLoginStatus(){
                      
                     }else{
 
-            
-                        cartData.playerId.push(userId);
+                        cartData.playerId.push({userId: userId, jmlDm: 0});
                         localStorage.setItem('cart', JSON.stringify(cartData));
-
 
                         Swal.fire({
                             title: 'Berhasil!',
-                            text: "Berhasil menambahkan player Id!",
+                            text: "Berhasil menambahkan User ID!",
                             icon: 'success',
                             showCancelButton: false,
                             confirmButtonColor: '#3085d6',
